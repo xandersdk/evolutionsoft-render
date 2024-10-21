0813005f8dca851000527dd8ddfbea06b653ff23
LISENA MARISOL QUILUMBA YUGCHA <lisena.quilumba4240@utc.edu.ec>
2024-10-21 10:59:09 -0500

    Correccion de modelos

--- Aplicaciones/Reporte/admin.py
+++ Aplicaciones/Reporte/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from.models import Empresa,Empleado,Encargado,Certificado
+
 
 # Register your models here.
+admin.site.register(Empresa)
+admin.site.register(Empleado)
+admin.site.register(Encargado)
+admin.site.register(Certificado)
