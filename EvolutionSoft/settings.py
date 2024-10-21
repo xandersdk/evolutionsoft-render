76e2b2ec72df4574ebbab18dbc2607a7145b9321
Pablo Pullopaxi <pablo.pullopaxi0913@gmail.com>
2024-10-21 11:08:03 -0500

    configuracion carpeta media

--- EvolutionSoft/settings.py
+++ EvolutionSoft/settings.py
@@ -118,6 +118,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS= (os.path.join(BASE_DIR,'EvolutionSoft/static'),)
+MEDIA_URL='/media/'
+MEDIA_ROOT=(os.path.join(BASE_DIR,'EvolutionSoft/media/'))
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
