821334ab88888af20553943043eda1930adc43f1
Pablo Pullopaxi <pablo.pullopaxi0913@gmail.com>
2024-10-21 13:59:53 -0500

    editar

--- Aplicaciones/Reporte/urls.py
+++ Aplicaciones/Reporte/urls.py
@@ -7,5 +7,8 @@ urlpatterns=[
     path('',views.home),
     path('listadoEmpresas/', views.listadoEmpresas, name='listadoEmpresas'),
     path('nuevaEmpresa/', views.nuevaEmpresa, name='nuevaEmpresa'),
-    path('guardarEmpresa/', views.guardarEmpresa)
+    path('guardarEmpresa/', views.guardarEmpresa),
+    path('eliminarEmpresa/<id>', views.eliminarEmpresa),
+    path('editarEmpresa/<id>', views.editarEmpresa),
+    path('procesarActualizacionEmpresa/', views.procesarActualizacionEmpresa, name='procesarActualizacionEmpresa'),
 ]
--- Aplicaciones/Reporte/views.py
+++ Aplicaciones/Reporte/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from .models import Empresa
 from django.contrib import messages
 
+
 # Create your views here.
 # Create your views here.
 def home(request):
@@ -36,7 +37,37 @@ def nuevaEmpresa(request):
     return render(request, 'nuevaEmpresa.html')
 
 def eliminarEmpresa(request, id):
-    empresaEliminar = Empresa.objects.get(id_=id)
+    empresaEliminar = Empresa.objects.get(id=id)
     empresaEliminar.delete()
-    messages.success(request, 'Jugador eliminado exitosamente')
+    messages.success(request, 'Empresa eliminado exitosamente')
+    return redirect('listadoEmpresas')
+
+def editarEmpresa(request, id):
+    empresaEditar = Empresa.objects.get(id=id)
+    return render(request, 'editarempresa.html', {'empresaEditar': empresaEditar})
+
+# Actualizando los nuevos datos en la base de datos
+def procesarActualizacionEmpresa(request):
+    if request.method == 'POST':
+        id = request.POST['id']
+        ruc = request.POST['ruc']
+        nombre = request.POST['nombre_empresa']
+        descripcion = request.POST['descripcion']
+        logo= request.FILES.get("foto") # Obtener el archivo de la foto
+
+        try:
+            empresaConsultado = Empresa.objects.get(id=id)
+            empresaConsultado.ruc = ruc
+            empresaConsultado.nombre = nombre
+            empresaConsultado.descripcion = descripcion
+
+            if logo:
+                empresaConsultado.logo = logo  # Actualizar la foto solo si se proporciona un nuevo archivo
+
+            empresaConsultado.save()
+
+            messages.success(request, 'Cliente actualizado con éxito')
+        except Empresa.DoesNotExist:
+            messages.error(request, 'El Cliente no existe')
+
     return redirect('listadoEmpresas')
