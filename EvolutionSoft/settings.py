367a2fc8e293197a940d6d02b1d79515a4195b1f
alexander <alejosdk@gmail.com>
2024-10-24 17:57:21 -0500

    c

--- EvolutionSoft/settings.py
+++ EvolutionSoft/settings.py
@@ -117,7 +117,8 @@ STATIC_URL = '/static/'
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
 
 # Static files in development
 STATICFILES_DIRS = [
