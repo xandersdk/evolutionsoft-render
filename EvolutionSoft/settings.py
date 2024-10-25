0ac1ca455848c00fc4da63aa1b0d6ba1dda2426e
alexander <alejosdk@gmail.com>
2024-10-24 23:47:50 -0500

    cambio

--- EvolutionSoft/settings.py
+++ EvolutionSoft/settings.py
@@ -114,27 +114,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'EvolutionSoft/static')]
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-# Static files in development
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'EvolutionSoft/static'),
-]
-
-# Media files (uploaded files)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'EvolutionSoft/media/')
 
-# Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Security settings
-SECURE_SSL_REDIRECT = not DEBUG
-CSRF_COOKIE_SECURE = not DEBUG
-SESSION_COOKIE_SECURE = not DEBUG
-
-WHITENOISE_MANIFEST_STRICT = False
